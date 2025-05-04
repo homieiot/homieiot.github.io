bf45a8f600d5c2bf78cb3e92a46c8130fd6b9625
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-05-04 13:51:33 +0200

    renames Dockerfile to clarify what kind of image it produces

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # As long as no changes are made to the Dockerfile, the built image
 # will be cached and reused in all builds after the first one.
-docker build -t homie-website-builder -f ./docker/Dockerfile .
+docker build -t homie-website-builder -f ./docker/Dockerfile-builder .
 
 # No need for a dedicated entrypoint script, we can specify the commands
 # to run as arguments to `docker run`. This keeps our image more flexible.
