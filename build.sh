a805233ebac117b26ea80e8c71c1d00b89515054
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-05-04 13:07:53 +0200

    moves to a docker-based, reproducible build environment

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -euo pipefail
 
-echo "Manually building webpage"
+# As long as no changes are made to the Dockerfile, the built image
+# will be cached and reused in all builds after the first one.
+docker build -t homie-website-builder -f ./docker/Dockerfile .
 
-python3 -m venv dependencies
-source dependencies/bin/activate
-pip install -r requirements.txt
-./grabrepos.py
-hugo
-
-echo "Webpage available at ./site"
+# No need for a dedicated entrypoint script, we can specify the commands
+# to run as arguments to `docker run`. This keeps our image more flexible.
+docker run --rm -v "$(pwd):/data" homie-website-builder sh -c "./grabrepos.py && hugo"
