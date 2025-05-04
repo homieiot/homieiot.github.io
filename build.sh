894126c576ed30afd10384c51d52c9d1b9614459
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-05-04 13:49:32 +0200

    no need to explicitly declare a volume within the Dockerfile

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ docker build -t homie-website-builder -f ./docker/Dockerfile .
 
 # No need for a dedicated entrypoint script, we can specify the commands
 # to run as arguments to `docker run`. This keeps our image more flexible.
-docker run --rm -v "$(pwd):/data" homie-website-builder sh -c "./grabrepos.py && hugo"
+docker run --rm -v "$(pwd):/data" homie-website-builder sh -c "cd /data && ./grabrepos.py && hugo"
